857ed7bd62b8b917bcfbf9468bbb9db469ab1e54
Annie <anqi.chen@thoughtworks.com>
2020-08-10 16:43:12 +0800

    [anqi] commit practice-4

--- 1-string/startsWith/index.js
+++ 1-string/startsWith/index.js
@@ -1,3 +1,10 @@
 export default function collectCarNumberCount(collection) {
   // TODO 7: 在这里写实现代码
+  let result = 0;
+  collection.forEach(element => {
+    if (/^粤A/.test(element)) {
+      result += 1;
+    }
+  });
+  return result;
 }
